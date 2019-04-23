e4f9f3ff77f875351b5253619996ef7390bd2591
fizoratti <robson.felipe@acad.pucrs.br>
2019-04-23 20:23:59 -0300

    Implements Factory

--- src/App.java
+++ src/App.java
@@ -5,12 +5,13 @@ import java.util.List;
 public class App {
     public static void main(String args[]) {
 
-        Message message = new Message("Hello World!");
-        System.out.println(message.getMessage());
+        AbstractFactory factory = new ConcreteFactory();
+        factory.printScreen();
+        factory.printFile();
 
-        Scanner ler = new Scanner(System.in);
+        System.out.println("(1) na tela ou (2) num arquivo");
 
-        String texto = ler.next();
+        String option = (new Scanner(System.in)).next();
 
         ler.close();
 
