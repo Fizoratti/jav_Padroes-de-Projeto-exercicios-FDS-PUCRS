996b34ffc51a946fa27a1578a98eaf5aedd1522b
fizoratti <robson.felipe@acad.pucrs.br>
2019-05-16 20:12:12 -0300

    Makes the option selection random

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
+import java.lang.Math;
 
 public class App {
     public static void main(String args[]) {
@@ -9,10 +10,19 @@ public class App {
 
         System.out.println("(1) na tela ou (2) num arquivo");
 
+        /* Pulando a opção de escolher o método
         int option = (new Scanner(System.in)).nextInt();
+        */
 
-        if(option == 1) System.out.println("Hello World");
-        if(option == 2) factory.printFile();
+        System.out.println("Gerando opcao randomica...");
+
+        double option = Math.random();
+
+        if(option > 0.5) System.out.println("Hello World");
+        if(option <= 0.5) factory.printFile();
+
+        if(option > 0.5) System.out.println("Escrito na tela.");
+        if(option <= 0.5) System.out.println("Escrito no arquivo.");
         
     }
 }
