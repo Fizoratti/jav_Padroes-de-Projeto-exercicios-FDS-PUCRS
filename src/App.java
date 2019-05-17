09a4e317615353bd479d231b66be7b77ec1aa6e4
fizoratti <robson.felipe@acad.pucrs.br>
2019-05-16 21:01:23 -0300

    Start implementing the date as argument

--- src/App.java
+++ src/App.java
@@ -1,30 +1,40 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
-import java.lang.Math;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class App {
     public static void main(String args[]) {
 
         AbstractFactory factory = new ConcreteFactory();
 
-        System.out.println("# Exercício 1.1");
+        int dia = args[0];
+        int mes = args[1];
+        int ano = args[2];
 
-        System.out.println("(1) na tela ou (2) num arquivo");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-mm-yyyy");
+        Date data = simpleDateFormat.parse(dia+"-"+mes+"-"+ano);
 
-        /* Pulando a opção de escolher o método
-        int option = (new Scanner(System.in)).nextInt();
-        */
+        System.out.println("# Exercício 1.2");
 
-        System.out.println("Gerando opcao randomica...");
+        
 
-        double option = Math.random();
+        // System.out.println("(1) na tela ou (2) num arquivo");
 
-        if(option > 0.5) System.out.println("Hello World");
-        if(option <= 0.5) factory.printFile();
+        // /* Pulando a opção de escolher o método
+        // int option = (new Scanner(System.in)).nextInt();
+        // */
 
-        if(option > 0.5) System.out.println("Escrito na tela.");
-        if(option <= 0.5) System.out.println("Escrito no arquivo.");
+        // System.out.println("Gerando opcao randomica...");
+
+        // double option = Math.random();
+
+        // if(option > 0.5) System.out.println("Hello World");
+        // if(option <= 0.5) factory.printFile();
+
+        // if(option > 0.5) System.out.println("Escrito na tela.");
+        // if(option <= 0.5) System.out.println("Escrito no arquivo.");
         
     }
 }
