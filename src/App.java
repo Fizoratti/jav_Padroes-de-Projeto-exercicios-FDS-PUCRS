44ef995748e5db4c87e8b6040c929d5038b21435
Fizoratti <robson.felipe@acad.pucrs.br>
2019-05-18 12:24:12 -0300

    Fix a Sop

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ import java.lang.Math;
 public class App {
     public static void main(String args[]) {
 
-        System.out.println("# Exercício 1.1");
+        System.out.println("# Exercício 2.1");
 
         Scanner sc = new Scanner(System.in);
 		
