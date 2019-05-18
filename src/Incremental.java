f917a83d65bf2989ae34d0fa8f8f54631f87a5e2
Fizoratti <robson.felipe@acad.pucrs.br>
2019-05-18 12:54:24 -0300

    Make Incremental a Singleton

--- src/Incremental.java
+++ src/Incremental.java
@@ -1,10 +1,17 @@
 class Incremental {
-    private static int count = 0;
-    private int numero;
-    public Incremental() {
-       numero = ++count;
-    }
-    public String toString() {
-       return "Incremental " + numero;
-    }
- }
+   private static Incremental INSTANCE = new Incremental();
+   private static int count = 0;
+   private int numero;
+
+   public Incremental() {
+      numero = ++count;
+   }
+
+   public static Incremental getInstance() {
+      return INSTANCE;
+   }
+
+   public String toString() {
+      return "Incremental " + numero;
+   }
+}
